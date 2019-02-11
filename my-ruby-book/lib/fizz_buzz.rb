8cffe2d8a6e99adca82131a5968bb907a7e2713e
Zacky <scarecrow.gang.duck@gmail.com>
2019-02-11 20:02:33 +0900

    Add minitest/fizz_buzz_test.rb

--- my-ruby-book/lib/fizz_buzz.rb
+++ my-ruby-book/lib/fizz_buzz.rb
@@ -6,19 +6,6 @@ def fizz_buzz(n)
     elsif n % 5 == 0
         'Buzz'
     else
-        n
+        n.to_s
     end
 end
-
-num_array = [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15]
-
-for n in num_array
-    puts fizz_buzz(n)
-end
-
-# puts fizz_buzz(1)
-# puts fizz_buzz(2)
-# puts fizz_buzz(3)
-# puts fizz_buzz(4)
-# puts fizz_buzz(5)
-# puts fizz_buzz(15)
